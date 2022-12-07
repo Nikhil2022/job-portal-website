774e772f2bffbceb19649a1c3723b2cd3f4d9581
naxx-gg <114809325+naxx-gg@users.noreply.github.com>
2022-12-07 22:24:40 +0530

    Delete user directory

--- user/config/init.php
+++
@@ -1,7 +0,0 @@
-<?php
-//start session
-session_start();
-//include helper file
-require_once 'helpers/system_helpers.php';
-require_once 'config.php';
-
--- user/templates/inc/footer.php
+++
@@ -1,7 +0,0 @@
-<footer class="footer">
-        <p>&copy; 2022 JobSeeker, Inc.</p>
-      </footer>
-    </div>
-
-    </body>
-</html>
